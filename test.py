50a0619bca86a943de38880c3b6992096444d09e
jigneshhack125 <140614058+jigneshhack125@users.noreply.github.com>
2023-07-28 13:05:08 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,10 @@
+from time import sleep
+import keyboard
+
+keyboard.write("ls")
+sleep(1)
+keyboard.press('enter')
+sleep(1)
+keyboard.write(" humannotexist3.jpg")
+keyboard.press('enter')
+print("here is a photo of jignesh")
